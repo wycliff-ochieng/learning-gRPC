a5c2b5335b526f3c31bbf9828aea60371372f944
wycliff-ochieng <wyckieochieng91@gmail.com>
2025-06-29 20:10:08 +0300

    grpc server setup

--- proto-project/server.go
+++ proto-project/server.go
@@ -1,8 +1,11 @@
 package main
 
 import (
-	pb "github.com/wycliff-ochieng/proto-project/coffee_proto"
 	"context"
+	"log"
+	"net"
+
+	pb "github.com/wycliff-ochieng/proto-project/coffee_proto"
 	"google.golang.org/grpc"
 )
 
@@ -10,24 +13,49 @@ type Server struct {
 	pb.UnimplementedCoffeeShopServer
 }
 
-func (s *Server) GetMenu(menuRequest *pb.MenuRequest, srv grpc.ServerStreamingServer[pb.Menu]) error {
-	items := *[]pb.Items{
-		&pb.Items{"id":"2","name":"Strong Tea"},
-		&pb.Items{"id":"3","name":"Mursik"},
-		&pb.Items{"id":"4","name":"white Tea"},
+func (s *Server) GetMenu(menuRequest *pb.MenuRequest, srv pb.CoffeeShop_GetMenuServer) error {
+	items := []*pb.Items{
+		&pb.Items{Id: "2", Name: "Strong Tea"},
+		&pb.Items{Id: "3", Name: "Mursik"},
+		&pb.Items{Id: "4", Name: "white Tea"},
 	}
 
-	for i,_ := range items{
+	for i, _ := range items {
 		srv.Send(&pb.Menu{
-			Items:items[0 : i+1],
+			Item: items[0 : i+1],
 		})
 	}
 	return nil
 }
 
-func (s *Server) PlaceOrder(context.Context, *pb.Order) (*pb.Receipt, error) {
-	return nil, nil
+func (s *Server) PlaceOrder(context context.Context, order *pb.Order) (*pb.Receipt, error) {
+	return &pb.Receipt{
+		Item: []*pb.Items{
+			{Id: "1", Name: "strong tea"},
+		},
+	}, nil
 }
-func (s *Server) GetOrderStatus(context.Context, *pb.Receipt) (*pb.OrderStatus, error) {
-	return nil, nil
+func (s *Server) GetOrderStatus(context context.Context, receipt *pb.Receipt) (*pb.OrderStatus, error) {
+	var orderId string
+	return &pb.OrderStatus{
+		OrderId: orderId,
+		Status:  "IN PROGRESS",
+	}, nil
+}
+
+func main() {
+
+	lis, err := net.Listen("tcp", ":9000")
+	if err != nil {
+		log.Fatalf("failed to listen:%v", err)
+	}
+
+	grpcServer := grpc.NewServer()
+
+	pb.RegisterCoffeeShopServer(grpcServer, &Server{})
+
+	if err = grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve : %s", err)
+	}
+
 }
